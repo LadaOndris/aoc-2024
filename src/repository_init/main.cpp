e1cb0499ae17193f43e1ae062eb182d3bdb027b3
Ladislav Ondris <ladislav.ondris@seznam.cz>
2025-10-28 15:04:01 +0100

    Refactored and fixed repository_init

--- src/repository_init/main.cpp
+++ src/repository_init/main.cpp
@@ -1,62 +1,98 @@
 
-#include <iostream>
-#include <fstream>
+#include <cstdlib>
 #include <filesystem>
 #include <format>
+#include <fstream>
+#include <iostream>
 
 namespace fs = std::filesystem;
 
 namespace {
-    struct InputArgs {
-        int startDay;
-        int endDay;
-        bool areInvalid;
-    };
 
-    InputArgs parseArguments(int argc, char *argv[]) {
-        if (argc != 3) {
-            return {.areInvalid = true};
-        }
-        InputArgs args{};
-        try {
-            args.startDay = std::stoi(argv[1]);
-            args.endDay = std::stoi(argv[2]);
-            args.areInvalid = false;
-        }
-        catch (...) {
-            args.areInvalid = true;
-        }
-        return args;
+struct InputArgs {
+    std::string argv0;
+    int startDay;
+    int endDay;
+    bool areInvalid;
+};
+
+InputArgs parseArguments(int argc, char* argv[]) {
+    if (argc != 3) {
+        return {
+            .argv0 = argv[0], .startDay = -1, .endDay = -1, .areInvalid = true};
+    }
+    InputArgs args{};
+    try {
+        args.argv0 = argv[0];
+        args.startDay = std::stoi(argv[1]);
+        args.endDay = std::stoi(argv[2]);
+        args.areInvalid = false;
+    } catch (...) {
+        args.areInvalid = true;
+    }
+    return args;
+}
+
+class TemplateHandler {
+   public:
+    TemplateHandler(std::string_view argv0)
+        : executablePath(getExecutablePath(argv0)) {}
+
+    fs::path getTemplatePath() const {
+        return executablePath / "main_template.cpp";
     }
 
-    std::optional<fs::path> navigateToParentDirectory(const std::string &targetDirName) {
-        auto path = fs::current_path();
+   private:
+    fs::path executablePath;
 
-        while (true) {
-            // Check if the current directory matches the target directory name
-            if (path.filename() == targetDirName) {
-                std::cout << "Found target directory: " << path << std::endl;
-                return path; // Return the path to the target directory
-            }
-
-            // Move up one directory
-            if (path == path.root_path()) {
-                // We've reached the root directory; stop searching
-                break;
-            }
-            path = path.parent_path();
+    static fs::path getExecutablePath(std::string_view argv0) {
+        fs::path exePath(argv0);
+        if (exePath.is_relative()) {
+            exePath = fs::current_path() / exePath;
         }
+        return exePath.parent_path();
+    }
+};
 
-        std::cout << "Target directory not found." << std::endl;
-        return std::nullopt; // Return no path
+class FileCreator {
+   public:
+    explicit FileCreator(const fs::path& root) : rootPath(root) {}
+
+    void createEmptyInputFiles(int firstDay, int lastDay) const {
+        auto folderPath = rootPath / "input_files";
+        constexpr auto fileNameFormat = "day{:02}.txt";
+
+        for (int i = firstDay; i <= lastDay; ++i) {
+            std::string fileName = std::format(fileNameFormat, i);
+            createEmptyFile(folderPath, fileName);
+        }
     }
 
-    void createEmptyFile(const std::string &folderPath, const std::string &fileName) {
+    void createSourceFilesFromTemplate(
+        int firstDay, int lastDay,
+        const TemplateHandler& templateHandler) const {
+        auto templatePath = templateHandler.getTemplatePath();
+        if (!fs::exists(templatePath)) {
+            std::cerr << "Template file does not exist: " << templatePath
+                      << std::endl;
+            return;
+        }
+
+        for (int i = firstDay; i <= lastDay; ++i) {
+            createSourceFileFromTemplate(i, templatePath);
+        }
+    }
+
+   private:
+    fs::path rootPath;
+
+    static void createEmptyFile(const fs::path& folderPath,
+                                const std::string& fileName) {
         if (!fs::exists(folderPath)) {
             fs::create_directory(folderPath);
             std::cout << "Created folder: " << folderPath << '\n';
         }
-        std::string filePath = fs::path(folderPath) / fileName;
+        fs::path filePath = folderPath / fileName;
         std::ofstream file(filePath);
         if (file) {
             std::cout << "Created file: " << filePath << '\n';
@@ -65,62 +101,67 @@ namespace {
         }
     }
 
-    void createEmptyInputFiles(int firstDay, int lastDay, const fs::path &rootPath) {
-        auto folderPath = rootPath / "input_files";
-        constexpr auto fileNameFormat = "day{:02}.txt";
-
-        for (int i = firstDay; i <= lastDay; i++) {
-            auto fileName = std::format(fileNameFormat, i);
-            createEmptyFile(folderPath, fileName);
-        }
-    }
-
-    void createSourceFileFromTemplate(int dayNumber, const fs::path &rootPath) {
-        std::string templatePath = "main_template.cpp"; // This should be located next to the binary
-
-        if (!std::filesystem::exists(templatePath)) {
-            std::cerr << "Template file does not exist: " << templatePath << std::endl;
-            return;
-        }
+    void createSourceFileFromTemplate(int dayNumber,
+                                      const fs::path& templatePath) const {
         auto targetDirPath = rootPath / std::format("src/day{:02}", dayNumber);
         auto targetFilePath = targetDirPath / "main.cpp";
 
         try {
-            // Create the directory if it doesn't exist
-            std::filesystem::create_directories(targetDirPath);
-
-            // Copy the template to the target file
-            std::filesystem::copy_file(templatePath, targetFilePath, std::filesystem::copy_options::skip_existing);
-
+            fs::create_directories(targetDirPath);
+            fs::copy_file(templatePath, targetFilePath,
+                          fs::copy_options::skip_existing);
             std::cout << "Template copied to: " << targetFilePath << std::endl;
-        } catch (const std::filesystem::filesystem_error &e) {
+        } catch (const fs::filesystem_error& e) {
             std::cerr << "Filesystem error: " << e.what() << std::endl;
-        } catch (const std::exception &e) {
+        } catch (const std::exception& e) {
             std::cerr << "Error: " << e.what() << std::endl;
         }
     }
+};
 
-    void createSourceFilesFromTemplate(int firstDay, int lastDay, const fs::path &rootPath) {
-        for (int i = firstDay; i <= lastDay; i++) {
-            createSourceFileFromTemplate(i, rootPath);
+std::optional<fs::path> navigateToParentDirectory(
+    const std::string& targetDirName) {
+    auto path = fs::current_path();
+
+    while (true) {
+        if (path.filename() == targetDirName) {
+            std::cout << "Found target directory: " << path << std::endl;
+            return path;
         }
+        if (path == path.root_path()) {
+            break;
+        }
+        path = path.parent_path();
     }
+
+    std::cout << "Target directory not found." << std::endl;
+    return std::nullopt;
 }
 
+}  // namespace
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     InputArgs args{parseArguments(argc, argv)};
     if (args.areInvalid) {
         std::cout << "Input arguments are invalid." << std::endl;
-        return 1;
+        std::cout << "Usage: repository_init <start_day> <end_day>"
+                  << std::endl;
+        return EXIT_FAILURE;
     }
 
     auto rootDirectory = navigateToParentDirectory("aoc-2024");
     if (!rootDirectory.has_value()) {
-        std::cout << "Could not navigate to the root folder of the repository." << std::endl;
-        return 1;
+        std::cout << "Could not navigate to the root folder of the repository."
+                  << std::endl;
+        return EXIT_FAILURE;
     }
 
-    createEmptyInputFiles(args.startDay, args.endDay, rootDirectory.value());
-    createSourceFilesFromTemplate(args.startDay, args.endDay, rootDirectory.value());
+    TemplateHandler templateHandler(args.argv0);
+    FileCreator fileCreator(rootDirectory.value());
+
+    fileCreator.createEmptyInputFiles(args.startDay, args.endDay);
+    fileCreator.createSourceFilesFromTemplate(args.startDay, args.endDay,
+                                              templateHandler);
+
+    return EXIT_SUCCESS;
 }
